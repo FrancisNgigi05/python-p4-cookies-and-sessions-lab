cf0e57c0fed317f29ad8db854381c8dfce6aa03d
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-04-23 18:00:12 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -22,19 +22,42 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-    articles = [article.to_dict() for article in Article.query.all()]
-    return make_response(jsonify(articles), 200)
+    # Where all the db data will be stored
+    data  = []
+    # Getting all the articles in the db
+    for article in Article.query.all():
+        # Transform the each db data to a dict
+        article_dict = article.to_dict()
+        # Appending the db data
+        data.append(article_dict)
+        # Converting the dict to json format
+        response = make_response(jsonify(data),  200)
+    # Getting all of the response needed
+    return response
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    session['page_views'] = session.get('page_views') or 0
-    session['page_views'] += 1
+    """Start by acknowledging that there is a a key page_views which should have a value during get
+    if there is no such key then create the key and set the value to zero"""
+    session['page_views'] = session.get("page_views") or 0
+    session["page_views"] += 1
 
-    if session['page_views'] <= 3:
-        return Article.query.filter(Article.id == id).first().to_dict(), 200
+    if session["page_views"] > 3:
+        # setting the limit of amnt of get requests on the blog site
+        response_body = {"message": "Maximum pageview limit reached"}
+        # Making the response_body to json data
+        response = make_response(jsonify(response_body), 401)
+
+        return response
     
-    return {"message": "Maximum pageview limit reached"}, 401
+    # If the limit is not reached the user can still view the article
+    article = Article.query.filter(Article.id == id).first()
+    # Converting the db data to a dict
+    article_dict = article.to_dict()
+    # Converting the dict to json format
+    response  = make_response(jsonify(article_dict), 200)
 
+    return response
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
